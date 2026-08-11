57302d096e5ea76f03a6804afa68625c774ba504
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-08-11 13:57:51 +0900

    Point the viewer assets at the left-aligned stylesheet

--- src/BindingsHtml.php
+++ src/BindingsHtml.php
@@ -72,7 +72,7 @@ use const JSON_UNESCAPED_SLASHES;
  */
 final class BindingsHtml
 {
-    private const VERSION = '4d0b285e5379aa6ed44cb1c5a2a80aa087a7259a';
+    private const VERSION = '0ec7d2247d49b7e2c6762989678c9d45012b2e15';
     public const CSS_URL = 'https://cdn.jsdelivr.net/gh/ray-di/Ray.Bindings@' . self::VERSION . '/docs/bindings/bindings.css';
     public const JS_URL = 'https://cdn.jsdelivr.net/gh/ray-di/Ray.Bindings@' . self::VERSION . '/docs/bindings/bindings.js';
 
